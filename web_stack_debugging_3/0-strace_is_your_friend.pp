216078d3b33236f8ed172ab256e6c6dc448794ce
frankmusiime <f.musiime@alustudent.com>
2025-07-26 00:01:08 +0000

    friend

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Puppet manifest to fix Apache 500 error by restoring the missing WordPress symlink
+# This Puppet manifest fixes a 500 error by restoring the symlink from /var/www/html to /var/www/wordpress, ensuring WordPress loads correctly
 
 file { '/var/www/html':
   ensure => 'link',
@@ -9,4 +9,3 @@ service { 'apache2':
   ensure => running,
   enable => true,
 }
-
