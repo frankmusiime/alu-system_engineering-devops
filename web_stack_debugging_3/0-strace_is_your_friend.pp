369b31264bf64e1ea86f185d3853e705d6c78fff
frankmusiime <f.musiime@alustudent.com>
2025-07-26 15:55:37 +0000

    my wordpress

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -4,8 +4,8 @@ package { 'php5-mysql':
   ensure => installed,
 }
 
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  subscribe => Package['php5-mysql'],
+exec { 'restart-apache':
+  command     => '/etc/init.d/apache2 restart',
+  refreshonly => true,
+  subscribe   => Package['php5-mysql'],
 }
